a2962335f0566613778581cc8eec65bb2c949c61
DJDarkCyber <danieljoe.2004@protonmail.ch>
2023-01-31 01:03:22 +0530

    Designed Front End

--- static/jsFiles/myJSFiles/actionPerform.js
+++ static/jsFiles/myJSFiles/actionPerform.js
@@ -1,19 +1,14 @@
 $(document).ready(function() {
 
     
-    $('#search-input').on('keyup', function(){
-        var search_query = $(this).val();
-
-        $.ajax({
-            url: '/editStudent',
-            data: {'search_query': search_query},
-            success: function(data){
-                $('#search-results').html(data);
-            }
-        });
-    });
+    
     
 
 
 
 });
+
+
+$(document).scroll(function() {
+    $("#header").toggleClass("scrolled", $(this).scrollTop() > 50);
+  });
