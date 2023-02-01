eabf2678e041352e1092b92eb54553f3f8a8f3eb
DJDarkCyber <danieljoe.2004@protonmail.ch>
2023-02-01 11:24:46 +0530

    Designed Front End

--- static/jsFiles/myJSFiles/actionPerform.js
+++ static/jsFiles/myJSFiles/actionPerform.js
@@ -2,7 +2,23 @@ $(document).ready(function() {
 
     
     
-    
+    // $(".web-showcase-events").hover(function(){
+    //   $(".showcase-events-desc").slideDown("fast");
+      
+    // },
+    // function(){
+    //   $(".showcase-events-desc").slideUp("fast", function(){
+      
+    //   });
+    // }
+    // );
+
+    $(".web-showcase-events").on('mouseenter touchstart', function(){
+      $(this).find(".showcase-events-desc").slideDown("fast");
+    }).on('mouseleave touchend', function(){
+      $(this).find(".showcase-events-desc").slideUp("fast");
+    }
+  );
 
 
 
